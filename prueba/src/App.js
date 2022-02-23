f62dd5f238202a5555722442cc30407ec112e8fc
Rocolas2000 <91926184+Rocolas2000@users.noreply.github.com>
2022-02-22 19:15:26 -0500

    useState Hooks

--- prueba/src/App.js
+++ prueba/src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [linea1, setLinea1] = useState('') ;
+  const [linea2, setLinea2] = useState('');
+
+
+const onChangeLinea1 = function(evento){
+  
+  alert(evento.target.value)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+
+        <select>
+          <option value="op1">op1</option>
+          <option value="op2">op2</option>
+          <option value="op3">op3</option>
+          <option value="op4">op4</option>
+          <option value="op5">op5</option>
+        </select> <br />
+
+        <input onChange={onChangeLinea1} type="text" placeholder='Linea 1' /> <br />
+        <input type="text" placeholder='Linea 2' /> <br />
+        
+        <button>Exportar</button>
+
+        <div>
+          <span> {linea1} </span> <br />
+          <span> {linea2} </span>
+          <img src=""></img>
+        </div>
+
+
     </div>
   );
 }
